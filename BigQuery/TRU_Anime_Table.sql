0bc285506ff49a3c8a5071d985dbf083d9f9017b
scudeller.lemos <scudeller.lemos@gmail.com>
2025-01-16 14:29:20 -0300

    Melhorias na tabela TRU

--- BigQuery/TRU_Anime_Table.sql
+++ BigQuery/TRU_Anime_Table.sql
@@ -2,5 +2,8 @@ CREATE OR REPLACE TABLE `personal-anime-data-2024.TRU_DATA.TRU_ANIMELIST` AS
   SELECT
     *
   FROM `personal-anime-data-2024.RAW_DATA.RAW_ANIMELIST`
-  --Filtar para entrar no Lake somente 3 anos de dados
-  WHERE SEASON_YEAR BETWEEN 2022 AND 2024;
+  -- Filtrar os últimos 3 anos com base no máximo de SEASON_YEAR
+  WHERE SEASON_YEAR >= (
+    SELECT MAX(SEASON_YEAR) - 2
+    FROM `personal-anime-data-2024.RAW_DATA.RAW_ANIMELIST`
+  );
