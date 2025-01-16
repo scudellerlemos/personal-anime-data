894d2729d5158e0cfaba946d3d93d2ae2b1bdf83
scudeller.lemos <scudeller.lemos@gmail.com>
2025-01-16 13:57:56 -0300

    Migraçao de ingestao para Pyspark

--- Ingestion/INGESTAO_FULL_RAW_DATA_ANIME_STORAGE_BQ.py
+++ Ingestion/INGESTAO_FULL_RAW_DATA_ANIME_STORAGE_BQ.py
@@ -1,11 +1,10 @@
-import gcsfs
-import pandas as pd
-from google.cloud import storage, bigquery
-from pandas_gbq import to_gbq
+from pyspark.sql import SparkSession
+from google.cloud import storage
 import os
-import chardet  # Importando o chardet para detecção de encoding
+import chardet
+import pandas as pd  # Para detectar encoding
 
-# Variáveis de Conta de Serviço e Bigquery
+# Variáveis de Conta de Serviço e BigQuery
 source_path = '#ACCOUNT SERVICE#'
 bucket_name = '#NIME_BUCKET_GCP#'  # Nome do bucket GCS
 file_name = '#FILE_NAME.csv#'  # Nome do arquivo no bucket
@@ -18,34 +17,41 @@ service_account_json = source_path
 # Define a variável de ambiente para a conta de serviço
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_json
 
+# Inicializa a SparkSession
+spark = SparkSession.builder.appName("GCS to BigQuery").getOrCreate()
+
 # Função para detectar o encoding do conteúdo de um arquivo em fluxo
 def detect_encoding_from_stream(file_stream):
     raw_data = file_stream.read(10000)  # Ler uma amostra do arquivo
     result = chardet.detect(raw_data)
     return result['encoding']
 
-# Carregar o arquivo diretamente do GCS em um DataFrame
+# Lê o arquivo do GCS
 print(f"Lendo o arquivo de {gcs_path}...")
 
-fs = gcsfs.GCSFileSystem(project=project_id)  # Conecta ao GCS
-with fs.open(gcs_path, "rb") as f:
-    # Detectar o encoding a partir do fluxo de dados do arquivo no GCS
+# Inicializa o cliente do Google Cloud Storage
+client = storage.Client()
+bucket = client.bucket(bucket_name)
+blob = bucket.blob(file_name)
+
+# Baixa o arquivo para detectar o encoding
+with blob.open("rb") as f:
     encoding = detect_encoding_from_stream(f)
     print(f"Encoding detectado: {encoding}")
 
-    # Reposiciona o ponteiro do arquivo para o início após a leitura da amostra
-    f.seek(0)
+# Recarrega o arquivo com o encoding detectado para um DataFrame PySpark
+with blob.open("r", encoding=encoding) as f:
+    pdf = pd.read_csv(f)  # Lê o arquivo em um DataFrame Pandas para conversão
 
-    # Agora, podemos carregar o arquivo com o encoding detectado
-    df = pd.read_csv(f, encoding=encoding)
+df = spark.createDataFrame(pdf)  # Converte para DataFrame PySpark
 
-# Carregar o DataFrame no BigQuery
+# Salva os dados no BigQuery
 print(f"Carregando dados na tabela {table_id} no BigQuery...")
-to_gbq(
-    dataframe=df,
-    destination_table=table_id,
-    project_id=project_id,
-    if_exists="replace",  # Cria a tabela ou substitui se ela já existir
-)
+df.write \
+    .format("bigquery") \
+    .option("table", table_id) \
+    .option("temporaryGcsBucket", bucket_name) \
+    .mode("overwrite") \
+    .save()
 
 print(f"Dados carregados com sucesso na tabela {table_id}!")
