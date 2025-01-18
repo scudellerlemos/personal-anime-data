9aee7e0df1cca09af78a10244d43d882407bc03e
scudeller.lemos <scudeller.lemos@gmail.com>
2025-01-18 20:02:06 -0300

    Mais ajustes para resolver conflitos

--- BigQuery/queries/TRU_Anime_Table.sql
+++ BigQuery/queries/TRU_Anime_Table.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TABLE `personal-anime-data-2024.TRU_DATA.TRU_ANIMELIST` AS
   SELECT
     *
   FROM `personal-anime-data-2024.RAW_DATA.RAW_ANIMELIST`
-  -- Filtrar os últimos 3 anos com base no máximo de SEASON_YEAR
+  -- Filtrar os últimos 5 anos com base no máximo de SEASON_YEAR
   WHERE SEASON_YEAR >= (
     SELECT MAX(SEASON_YEAR) - 4
     FROM `personal-anime-data-2024.RAW_DATA.RAW_ANIMELIST`
